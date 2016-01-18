91c3f6fb21005f0f80ccebbda741305e31f8cbeb
Seth Trowbridge <seth111@gmail.com>
2016-01-18 16:51:47 -0500

    process started

--- app.js
+++ app.js
@@ -5,19 +5,38 @@ var fs = require("fs");
 
 var Proc = {};
 Proc.Job = {};
+
+Proc.WriteJSON = fs.createWriteStream("myOutput.txt");
+
+
 Proc.Init = function()
 {
 	Proc.Job = {};
+	Proc.Job.Title = "";
+	Proc.Job.ID = "";
+	Proc.Job.URL = "";
+	Proc.Job.Date = "";
+	
+	Proc.Job.Taxonomy = {};
+	Proc.Job.Taxonomy.Division = {
+		company:"",
+		department:""
+	};
+	Proc.Job.Taxonomy.Category = {
+		family:"",
+		category:""
+	};
+	Proc.Job.Taxonomy.Location = {
+		country:"",
+		state:"",
+		city:""
+	};
 };
+
+
 
 var State = {};
 State.TagName = "";
-
-var options = {
-	host:"www.linkup.com",
-	path:"/xmlFeed.php?gzip=1&access=t3n89z&company=25135&feed=37bc2f75bf1bcfe8450a1a41c200g304"	
-};
-
 var saxStream = sax.createStream(true, {});
 saxStream.on("opentag", function (inNode) {
   State.TagName = inNode.name;
@@ -35,25 +54,20 @@ saxStream.on("error", function(inError)
 });
 
 
-
+var options = {
+	host:"www.linkup.com",
+	path:"/xmlFeed.php?gzip=1&access=t3n89z&company=25135&feed=37bc2f75bf1bcfe8450a1a41c200g304"	
+};
 http.get(options, function (inResponse)
 {  
 	var encoding = inResponse.headers['content-encoding'];
-
 	if (encoding == 'gzip')
 	{
 	  inResponse.pipe(zlib.createGunzip()).pipe(saxStream);
-	}
-	else if (encoding == 'deflate')
-	{
-	  inResponse.pipe(zlib.createInflate()).pipe(saxStream);
 	}
 	else
 	{
 	  inResponse.pipe(saxStream);
 	}
-
-
 	inResponse.on('error', console.error);
-
 });
