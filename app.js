2a985e87b0f1f5351a402fa95c25059198a7d09f
Seth Trowbridge <seth111@gmail.com>
2016-01-19 13:04:33 -0500

    encapulated routine for parsing feeds given urls

--- app.js
+++ app.js
@@ -4,149 +4,139 @@ var zlib = require("zlib");
 var fs = require("fs");
 
 
-var Proc = {};
-
-Proc.State = {};
-Proc.State.TagName = "";
 
 
-Proc.StreamJSON = fs.createWriteStream("c:/nodejs/feed/clients/wegmans/data.js");
-Proc.StreamSAX = sax.createStream(true, {});
-Proc.StreamGUnZip = zlib.createGunzip();
-
-Proc.Job = {};
-Proc.Init = function()
-{
-	Proc.Job = {};
-	Proc.Job.Title = "";
-	Proc.Job.ID = "";
-	Proc.Job.URL = "";
-	Proc.Job.Date = "";
-	
-	Proc.Job.TaxDiv = ["", "", ""];
-	Proc.Job.TaxCat = ["", "", ""];
-	Proc.Job.TaxLoc = ["", "", ""];
-};
-
-
-Proc.StreamSAX.on("opentag", function (inNode)
-{
-	Proc.State.TagName = inNode.name;
-	if(Proc.State.TagName == "job")
-	{
-		Proc.StreamJSON.write(JSON.stringify(Proc.Job)+",\n");
-		Proc.Init();
-	}
-});
-
-Proc.StreamSAX.on("text", function(inText)
-{
-	if(inText == "\n\t\t" || inText === "\n\t")
-		return;
-		
-	switch(Proc.State.TagName)
-	{
-		case "title":
-			Proc.Job.Title += inText;
-			break;
-		case "jobkey":
-			Proc.Job.ID += inText;
-			break;
-		case "applyurl":
-			Proc.Job.URL += inText;
-			break;
-		case "pubdate":
-			Proc.Job.Date += inText;
-			break;
-		
-		case "company":
-			Proc.Job.TaxDiv[0] += inText;
-			break;
-		case "department":
-			Proc.Job.TaxDiv[1] += inText;
-			break;
-		
-		case "category":
-			Proc.Job.TaxCat[0] += inText;
-			break;
-		case "specificcategory":
-			Proc.Job.TaxCat[1] += inText;
-			break;
-			
-		case "country":
-			Proc.Job.TaxLoc[0] += inText;
-			break;	
-		case "state":
-			Proc.Job.TaxLoc[1] += inText;
-			break;	
-		case "city":
-			Proc.Job.TaxLoc[2] += inText;
-			break;	
-	}
-});
-Proc.StreamSAX.on("error", function(inError){console.log(inError);});
-
-var options = {};
-options.host = "www.linkup.com";
-options.path = "/xmlFeed.php?gzip=1&access=t3n89z&company=25135&feed=37bc2f75bf1bcfe8450a1a41c200g304";
-http.get(options, function (inResponse)
-{  
-	var encoding = inResponse.headers['content-encoding'];
-	if (encoding == 'gzip')
-	{
-	  inResponse.pipe(Proc.StreamGUnZip).pipe(Proc.StreamSAX);
-	}
-	else
-	{
-	  inResponse.pipe(Proc.StreamSAX);
-	}
-	inResponse.on('error', function(inError)
-	{
-		console.log("http error! ", inError);
-	});
-});
-
-
-
-
-/*
 var Feed = {};
 
 Feed.Config = {};
-Feed.Config.PathFeed = "feed.txt";
+Feed.Config.PathConfig = "config.js";
 Feed.Config.PathData = "data.js";
 Feed.Config.PathDescriptions = "descriptions";
 
-//inPath is a file system directory that contains a feed.txt file and is where data.js and descriptions/ will be created
-Feed.Process = function(inPath)
+Feed.Methods = {};
+Feed.Methods.BuildFiles = function XMLToJSON(inPathXML, inPathJSON, inPathDescription)
 {
-	fs.readFile(inPath+"/"+Feed.Config.PathFeed, function(inError, inData)
+	var State = {};
+	State.TagName = "";
+	State.Job = false;
+	State.Desciption = "";
+	State.NewJob = function()
+	{
+		State.Job = {};
+		State.Job.Title = "";
+		State.Job.ID = "";
+		State.Job.URL = "";
+		State.Job.Date = "";
+		State.Job.TaxDiv = ["", "", ""];
+		State.Job.TaxCat = ["", "", ""];
+		State.Job.TaxLoc = ["", "", ""];		
+	};
+	
+	var Stream = {};
+	Stream.JSON = fs.createWriteStream(inPathJSON);
+	Stream.GUnZip = zlib.createGunzip();
+	Stream.SAX = sax.createStream(true, {});
+	Stream.SAX.on("error", console.log);
+	Stream.SAX.on("opentag", function (inNode)
+	{
+		State.TagName = inNode.name;
+		if(State.TagName === "job")
+		{
+			if(State.Job)
+			{
+				Stream.JSON.write(JSON.stringify(State.Job)+",\n");
+				fs.writeFile(inPathDescription+"/"+State.Job.ID+".html", State.Desciption);
+			}
+			else
+			{
+				Stream.JSON.write("var Feed = [");
+			}
+			
+			State.Description = "";
+			State.NewJob();
+		}
+	});
+	Stream.SAX.on("end", function()
+	{
+		Stream.JSON.write("{}];");
+		Stream.JSON.end();
+		
+		
+	});
+	Stream.SAX.on("text", function(inText)
+	{
+		if(inText === "\n\t\t" || inText === "\n\t")
+			return;
+			
+		switch(State.TagName)
+		{
+			case "title":
+				State.Job.Title += inText;
+				break;
+			case "jobkey":
+				State.Job.ID += inText;
+				break;
+			case "applyurl":
+				State.Job.URL += inText;
+				break;
+			case "pubdate":
+				State.Job.Date += inText;
+				break;
+			
+			case "company":
+				State.Job.TaxDiv[0] += inText;
+				break;
+			case "department":
+				State.Job.TaxDiv[1] += inText;
+				break;
+			
+			case "category":
+				State.Job.TaxCat[0] += inText;
+				break;
+			case "specificcategory":
+				State.Job.TaxCat[1] += inText;
+				break;
+				
+			case "country":
+				State.Job.TaxLoc[0] += inText;
+				break;	
+			case "state":
+				State.Job.TaxLoc[1] += inText;
+				break;	
+			case "city":
+				State.Job.TaxLoc[2] += inText;
+				break;	
+				
+			case "description":
+				State.Desciption += inText;
+				break;
+		}
+	});
+
+	http.get(inPathXML, function (inResponse)
+	{  
+		inResponse.on('error', console.log);		
+		
+		var encoding = inResponse.headers['content-encoding'];
+		if (encoding == 'gzip')
+		  inResponse.pipe(Stream.GUnZip).pipe(Stream.SAX);
+		else
+		  inResponse.pipe(Stream.SAX);
+	});
+}
+
+Feed.Methods.Process = function(inPath)
+{
+	fs.readFile(inPath+"/"+Feed.Config.PathConfig, "utf8", function(inError, inData)
 	{
 		if(inError)
 			throw inError;
 			
-		console.log(inData.toString());
+		var config = JSON.parse(inData);
+		
+		Feed.Methods.BuildFiles(config.Source, inPath+"/"+Feed.Config.PathData, inPath+"/"+Feed.Config.PathDescriptions);
+		
 	});
 };
 
-Feed.Process("c:/nodejs/feed/clients/wegmans");
-*/
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+Feed.Methods.Process(__dirname+"/clients/wegmans");
