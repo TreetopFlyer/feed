4a6e4c51efd82bb00b0513a5a5173d81dc08647b
Seth Trowbridge <seth111@gmail.com>
2016-10-05 15:43:50 -0400

    new views, csv parsing

--- app.js
+++ app.js
@@ -1,19 +1,17 @@
-var http = require("http");
-var sax = require("sax");
-var zlib = require("zlib");
-var fs = require("fs-extra");
-var express = require("express");
+var     http = require("http");
+var      sax = require("sax");
+var     zlib = require("zlib");
+var       fs = require("fs-extra");
+var  express = require("express");
 var template = require("string-template");
 
 
 var Feed = {};
-
 Feed.Config = {};
 Feed.Config.PathConfig = "config.json";
 Feed.Config.PathData = "data.js";
 Feed.Config.PathDescriptions = "descriptions";
 Feed.Config.PathClients = "clients";
-
 Feed.Methods = {};
 Feed.Methods.BuildFiles = function(inConfig, inPathJSON, inPathDescription)
 {
@@ -38,7 +36,7 @@ Feed.Methods.BuildFiles = function(inConfig, inPathJSON, inPathDescription)
 				{
 					Stream.JSON.write(inConfig.Delimiter);
 				}
-				Stream.JSON.write(template(JSON.stringify(inConfig.Row), State.Fields));
+				Stream.JSON.write(template(inConfig.Row, State.Fields));
 
 				fs.writeFile(inPathDescription+"/"+State.Fields[inConfig.Description.NameWith]+".html", State.Fields[inConfig.Description.PullFrom]);
 				State.Count++;
@@ -64,17 +62,26 @@ Feed.Methods.BuildFiles = function(inConfig, inPathJSON, inPathDescription)
 		State.Fields[State.TagName] = inText;
 	});
 
-	http.get(inConfig.Source, function (inResponse)
-	{  
-		inResponse.on('error', console.log);		
-		var encoding = inResponse.headers['content-encoding'];
-		if (encoding == 'gzip')
-		  inResponse.pipe(Stream.GUnZip).pipe(Stream.SAX);
-		else
-		  inResponse.pipe(Stream.SAX);
+	// clear out the job descriptions folder
+	fs.emptyDir(inPathDescription, function(inError)
+	{
+		if(!inError)
+		{
+			// setup 
+			// then build the new data
+			http.get(inConfig.Source, function (inResponse)
+			{  
+				inResponse.on('error', console.log);		
+				var encoding = inResponse.headers['content-encoding'];
+
+				if (encoding == 'gzip')
+					inResponse.pipe(Stream.GUnZip).pipe(Stream.SAX);
+				else
+					inResponse.pipe(Stream.SAX);
+			});
+		}
 	});
 }
-
 Feed.Methods.ExecuteConfig = function(inClientFolder)
 {
 	fs.readFile(inClientFolder+"/"+Feed.Config.PathConfig, "utf8", function(inError, inData)
@@ -91,12 +98,29 @@ Feed.Methods.ExecuteConfig = function(inClientFolder)
 
 
 var Server = express();
+Server.use("/clients/:client", function(inReq, inRes, inNext)
+{
+	inRes.header('Cache-Control', 'public, max-age=7200');
+    inRes.header('Access-Control-Allow-Origin', '*');
+    inRes.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    inRes.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+    if(inReq.method == 'OPTIONS'){
+        inRes.sendStatus(200);
+    }else{
+        inNext();
+    }
+});
 Server.use("/components", express.static(__dirname+"/components"));
 Server.use("/clients", express.static(__dirname+"/clients"));
 Server.get("/clients/:client/update", function(inReq, inRes, inNext)
 {
-	inRes.status(200).send("updating "+inReq.params.client);
 	Feed.Methods.ExecuteConfig(__dirname + "/" + Feed.Config.PathClients + "/" + inReq.params.client);
+	inRes.status(200).send("updating "+inReq.params.client);
+});
+Server.get("/clients/:client/create", function(inReq, inRes, inNext)
+{
+	fs.copy(__dirname + "/" + Feed.Config.PathClients + "/_template", __dirname + "/" + Feed.Config.PathClients + "/" + inReq.params.client);
+	inRes.status(200).send("creating "+inReq.params.client);
 });
 Server.get("/", function(inReq, inRes, inNext)
 {
---
+++ components/table.js
@@ -0,0 +1,178 @@
+angular.module("Tabular", [])
+.config(function($sceDelegateProvider) {
+    $sceDelegateProvider.resourceUrlWhitelist([
+        'http://localhost/**'
+    ]);
+})
+.factory("Download", ["$http", function($http)
+{
+    return function(inURL, inDone)
+    {
+        var data = {
+            Header: undefined,
+            Rows: [],
+            Uniques: [],
+        };
+        var config = {
+            step: function(inRow)
+            {
+                var i, j, k;
+                var cell;
+                var row;
+                var match;
+                var LUT;
+
+                row = inRow.data[0];
+
+                if(data.Header === undefined)
+                {
+                    data.Header = row;
+                    for(i=0; i<row.length; i++)
+                    {
+                        data.Uniques[i] = {
+                            Title:row[i],
+                            Uniques:[]
+                        }
+                    }
+                }
+                else
+                {
+                    data.Rows.push(row);
+                    for(i=0; i<row.length; i++)
+                    {
+                        cell = row[i];
+                        match = false;
+                        LUT = data.Uniques[i].Uniques;
+                        for(j=0; j<LUT.length; j++)
+                        {
+                            if(cell == LUT[j].Value)
+                            {
+                                match = true;
+                                LUT[j].Rows.push(row);
+                                break;
+                            }
+                        }
+                        if(!match)
+                        {
+                            LUT.push({
+                                Value: cell,
+                                Rows: [row]
+                            });
+                        }
+                    }
+                }
+            },
+            complete: function(){
+                inDone(data);
+            }
+        };
+        $http.get(inURL).then(function(inResponse)
+        {
+            Papa.parse(inResponse.data, config);
+        });
+    };
+}])
+.factory("Table", ["$http", function($http)
+{
+    var obj = {};
+    obj.Create = function(inURL)
+    {
+        var Table = {};
+        Table.Header = undefined;
+        Table.Rows = [];
+        Table.Visible = [];
+
+        Table.Column = -1;
+        Table.ColumnDirection = 1;
+
+        Table.PagingOptions = [20, 50, 100, 200];
+        Table.PerPage = Table.PagingOptions[0];
+        Table.Page = 0;
+        Table.PageMin = 0;
+        Table.PageMax = 0;
+        Table.PageRadius = 1;
+        Table.Pages = [];
+        Table.UpdatePagination = function()
+        {
+            Table.Pages = [];
+            Table.Page = 0;
+            for(var i=0; i<Math.ceil(Table.Rows.length / Table.PerPage)-1; i++)
+            {
+                Table.Pages.push({
+                    index: i,
+                    min: i*Table.PerPage,
+                    max: (i+1)*Table.PerPage,
+                    active: false
+                });
+            }
+            Table.Pages.push({
+                index: i,
+                min: i*Table.PerPage,
+                max: Table.Rows.length,
+                active: false
+            });
+            Table.SetPage(0);
+        };
+        Table.SetPage = function(inIndex)
+        {
+            Table.Page.active = false;
+            Table.Page = Table.Pages[inIndex];
+            Table.Page.active = true;
+            Table.Visible = [];
+            for(var i=Table.Page.min; i<Table.Page.max; i++)
+            {
+                Table.Visible.push(Table.Rows[i]);
+            }
+
+            Table.PageMin = Table.Page.index - Table.PageRadius;
+            if(Table.PageMin < 0)
+            {
+                Table.PageMin = 0;
+            }
+            Table.PageMax = Table.Page.index + Table.PageRadius;
+            if(Table.PageMin >= Table.Pages.length-1)
+            {
+                Table.PageMax = Table.Pages.length-1;
+            }
+        };
+        Table.InRange = function(inIndex)
+        {
+            if(inIndex >= Table.PageMin && inIndex <= Table.PageMax)
+            {
+                return true;
+            }
+            return false;
+        };
+        Table.SetSort = function(inIndex)
+        {
+            if(Table.Column == inIndex)
+            {
+                Table.ColumnDirection *= -1;
+            }
+            else
+            {
+                Table.Column = inIndex;
+                Table.ColumnDirection = 1;
+            }
+            
+            Table.Rows.sort(function(a, b)
+            {
+                var aProp = a[inIndex].toUpperCase();
+                var bProp = b[inIndex].toUpperCase();
+                return (aProp === bProp ? 0 : (aProp < bProp ? -Table.ColumnDirection : Table.ColumnDirection));
+            });
+            Table.UpdatePagination();
+        }
+
+
+        Table.Populate = function(inArrayHeader, inArrayRows)
+        {
+            Table.Header = inArrayHeader;
+            Table.Rows = inArrayRows;
+            Table.UpdatePagination();
+        };
+
+        return Table;
+    };
+    return obj;
+}]);
